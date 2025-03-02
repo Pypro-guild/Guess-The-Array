3bb1d2cc26e860c4fa26f80b285eca187b3dc944
Pypro-guild <rishi.vigranam@gmail.com>
2025-03-02 18:22:27 +0800

    Add guessTheArray.cpp via upload (6:21 2 March)
    Uploaded finalised code for maths investigation.

---
+++ guessTheArray.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <ctime>
+
+void check(int array1[], int array2[], int size, int value);
+
+int main(){
+    int value;
+    srand(time(NULL));
+
+    const int SIZE = 10;
+    int g1[SIZE], g2[SIZE];
+    
+    std::cout << "Enter a value 1-20: ";
+    std::cin >> value;
+
+    for (int i = 0; i < SIZE; i++){
+        g1[i] = (rand()%20) + 1;
+        g2[i] = (rand()%20) + 1;
+    }
+    std::cout << '\n';
+    std::cout << "Array1:";
+    for (int i = 0; i < SIZE; i++){
+        std::cout << g1[i] << ' ';
+    }
+    std::cout << '\n';
+
+    std::cout << "Array2:";
+    for (int i = 0; i < SIZE; i++){
+        std::cout << g2[i] << ' ';
+    }
+
+    check(g1, g2, SIZE, value);
+
+    return 0;
+}
+
+void check(int array1[], int array2[], int size, int value){
+    for (int i = 0; i < size; i++){
+        if (array1[i] == value){
+            for (int j = 0; j < size; j++){
+            
+                if (array1[i] == array2[j]){
+                    std::cout << '\n';
+                    std::cout << "You won! Thanks for playing GUESS THE ARRAY!!";
+                    break;
+                }
+                else{
+                    continue;
+                }
+            break;
+            }
+        }
+    }
+}
